dce871a48ac192b0cbbd74081b5cef4d2280dd77
Reid Horuff <reidhoruff@gmail.com>
2012-12-07 21:10:07 -0700

    wsgi fix

--- ReidHoruff/production_wsgi.py
+++ ReidHoruff/production_wsgi.py
@@ -3,5 +3,9 @@ import sys
 
 from django.core.handlers.wsgi import WSGIHandler
 
+path = '/home/reidhoruff/webapps/reidhoruff/ReidHoruff'
+if path not in sys.path:
+	sys.path.append(path)
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ReidHoruff.settings'
 application = WSGIHandler()
